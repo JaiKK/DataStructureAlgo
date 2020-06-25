ba64206f8f137f50b53cfe7cd4ac73de9d446eac
Jai <jaikuraria@gmail.com>
2020-06-25 16:45:51 +0530

    first commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+mainmake: main.cpp
+	g++ -o main main.cpp
+
+clean:
+	rm *.exe
---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+
+int main(){
+
+    int data[10] = {10, 15, 4, 20, 2, 12, 8, 9, 1, 30};
+    bool sort_flag = true;
+
+    std::cout << "array: " << sizeof(data) << ", data:" << sizeof(data[0]) << std::endl;
+
+    std::cout << "Before: ";
+    for(const int &n : data){
+        std::cout << n << " ";
+    }
+    std::cout << std::endl;
+
+//for(int j=0; j < 10; j++){
+while(true){
+    sort_flag = true;
+    for(int i=0; i < 10; i++){
+        if (data[i] > data[i+1]){
+            int tmp = data[i];
+            data[i] = data[i+1];
+            data[i+1] = tmp;
+            sort_flag = false;
+        }
+        std::cout << data[i] << " ";
+    }
+    std::cout << std::endl;
+    if(sort_flag == true) break;
+}
+
+
+    // for(const int &n : data){
+    //     std::cout << n << " ";
+    // }
+
+
+}
