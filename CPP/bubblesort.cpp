feaabd2cb792384d39575d8fed35fc52e7d67609
Jai <jaikuraria@gmail.com>
2020-06-30 15:57:30 +0530

    refactored bubblesort program

--- CPP/bubblesort.cpp
+++ CPP/bubblesort.cpp
@@ -1,39 +1,82 @@
 #include<iostream>
 
+template <class T>
+void swap(T &x, T &y){
+    T tmp = x;
+    x = y;
+    y = tmp;
+}
 
-int main(){
+template <class T>
+T *bubblesort(T *data){
 
-    int data[10] = {10, 15, 4, 20, 2, 12, 8, 9, 1, 30};
     bool sort_flag = true;
 
-    std::cout << "array: " << sizeof(data) << ", data:" << sizeof(data[0]) << std::endl;
-
-    std::cout << "Before: ";
-    for(const int &n : data){
-        std::cout << n << " ";
-    }
-    std::cout << std::endl;
-
-//for(int j=0; j < 10; j++){
-while(true){
-    sort_flag = true;
-    for(int i=0; i < 10; i++){
-        if (data[i] > data[i+1]){
-            int tmp = data[i];
-            data[i] = data[i+1];
-            data[i+1] = tmp;
-            sort_flag = false;
+    while(true){
+        sort_flag = true;
+        for(int i=0; i < 10 - 1; i++){
+            if (data[i] > data[i+1]){
+                swap(data[i] , data[i+1]);
+                sort_flag = false;
+            }
+            // std::cout << data[i] << " ";
         }
-        std::cout << data[i] << " ";
+        // std::cout << std::endl;
+        if(sort_flag == true) break;
     }
-    std::cout << std::endl;
-    if(sort_flag == true) break;
+
+    return data;
 }
 
 
-    // for(const int &n : data){
-    //     std::cout << n << " ";
-    // }
+int main(){
 
-    return 0;
+    int data[10] = {10, 15, 4, 28, 5, 19, 8, 9, 1, 30};
+    float dataf[10] = {10.7, 10.1, 4.0, 28.0, 5.2, 5.1, 10.5, 9.0, 1.0, 30.0};
+    char dataa[10] = {'f', 'd', 'e', 'c', 'm', 'g', 'j', 'l', 'k', 'a'};
+
+    // bool sort_flag = true;
+
+    // std::cout << "array: " << sizeof(data) << ", data:" << sizeof(data[0]) << std::endl;
+
+    // bubblesort(data);
+    int *p = bubblesort(data);
+    for(int i=0; i < 10; i++){
+        std::cout << p[i] <<  " ";
+    }
+    std::cout << std::endl;
+
+    float *f = bubblesort(dataf);
+    for(int i=0; i < 10; i++){
+        std::cout << f[i] << " ";
+    }
+    std::cout << std::endl;
+
+    char *a = bubblesort(dataa);
+    for(int i=0; i < 10; i++){
+        std::cout << a[i] << " ";
+    }
+    std::cout << std::endl;    
+
+
+/*
+    //for(int j=0; j < 10; j++){
+    while(true){
+        sort_flag = true;
+        for(int i=0; i < 10; i++){
+            if (data[i] > data[i+1]){
+                // int tmp = data[i];
+                // data[i] = data[i+1];
+                // data[i+1] = tmp;
+                swap(data[i] , data[i+1]);
+                sort_flag = false;
+            }
+            std::cout << data[i] << " ";
+        }
+        std::cout << std::endl;
+        if(sort_flag == true) break;
+    }
+*/
+
+     return 0;
 }
